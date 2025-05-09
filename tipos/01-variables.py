1eb79da47eaa922796c39dc97444bab179459e0d
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-09 02:18:52 -0400

    ADD: Se agrega contenido

---
+++ tipos/01-variables.py
@@ -0,0 +1,12 @@
+"""Introducción a Python"""
+nombre_curso = "ultima python"
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NombreCurso = " Chanchito"
+print(nombre_curso, nombre1, NOMBRE_CURSO, NombreCurso)
+
+alumnos = 5000  # Integer --> entero #
+puntaje = 9.9  # Float , punto flotante  #
+publicado = True
+
+print(alumnos, puntaje)
