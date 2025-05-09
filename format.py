a6aba8eb2a1c8035cca3dba9e66375d6fa5d8051
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-09 01:54:20 -0400

    ADD: Se agrega contenido

---
+++ format.py
@@ -0,0 +1,4 @@
+"""Introducción a Python"""
+chanchito = "Feliz"
+a = 1
+b = 2
--- intro.py
+++ intro.py
@@ -1,2 +1,4 @@
+"""Introducción a Python"""
+
 print("Hola Mundo Cruel")
-print("El Weta " * 4) # hacemos que imprima el string 4 veces con el operador * 4 
+print("El Weta " * 4) # hacemos que imprima el string 4 veces con el operador *4 #
