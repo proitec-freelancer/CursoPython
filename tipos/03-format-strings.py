e099b835acdc444fe040e24426b3596f41a1c61b
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-18 12:48:21 -0400

    ADD: Se agrega 03-format-strings.py

---
+++ tipos/03-format-strings.py
@@ -0,0 +1,10 @@
+nombre = "Luis"
+apellido = "Vasquez"
+
+# nombre_completo = nombre + " " + apellido // esta es la manera tradicional
+
+# esta es la manera moderna con el codigo formateado
+# dentro de las llaves se pueden hacer operaciones o hasta llamar indices
+# nombre_completo = f"{nombre} {apellido}"
+nombre_completo = f"{nombre[0]} {2 + 5}"
+print(nombre_completo)
