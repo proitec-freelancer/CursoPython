aa7173c8dacac2e4b69db62d0bd9f75b727beb42
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-18 22:29:34 -0400

    ADD: Se agregan nuevos archivos

--- tipos/06-numeros.py
+++ tipos/06-numeros.py
@@ -2,8 +2,12 @@ numero = 2  # entero -> integer
 decimal = 1.2  # Float -> punto flotante
 imaginario = 2 + 2j  # 2 +2i la i representa la raiz cuadrada de menos 1
 
-numero = numero + 2
-
+# numero = numero + 2
+# numero += 2
+numero += 5
+numero *= 5
+numero -= 5
+numero /= 5
 print("numero", numero)
 print(1 + 3)
 print(1 - 3)
