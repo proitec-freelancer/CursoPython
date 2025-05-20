c05ecfed2902813c26c28b3b1405fbc010fb6fc6
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-19 22:51:24 -0400

    FIX: 08-calculadora.py ADD:09-conversion-tipos.py

---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,18 @@
+# X = input("") # Siempre lo ingresado por el usuario sera en formato string
+# recodar que podemos tranformarlo con el siguiente metodo o funcion
+# a int con x = int(x)
+
+# int()
+# str() cualquier dato ingresado lo tranforma a string
+# float()
+# bool() cualquier dato ingresado lo tranforma a booleano , toma en cuenta que en python los booleanos son True o False
+# O son truthy y falsy
+# falsy toma los siguientes datos como falsos - "" (espacio vacio) - 0 - None - False
+# truthy toma los siguientes datos como verdaderos - "hola" - 1
+
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
