a27108aa0b8b82ce628d30489e455d89cc9b0497
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-23 14:27:07 -0400

    ADD: if.py

---
+++ control-flujo/02-if.py
@@ -0,0 +1,15 @@
+edad = 15
+if edad > 65:
+    print("Puede ver la pelicula con super descuento")
+
+elif edad > 54:
+    print("Puede ver la pelicula con descuento")
+
+elif edad > 17:
+    print("Puede ver la pelicula")
+
+else:
+    print("No puede ver la pelicula")
+    print("Ve a otro lado")
+
+print("****************listo********************")
